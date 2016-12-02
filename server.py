2e441c874191ee08bc2a49c90db8947d97400c6f
Boris Tseytlin <euphetar@gmail.com>
2016-12-02 18:05:31 +0300

    refactor code, add ability to leave rooms, add web client

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 import asyncio
 import websockets
 import uuid
+import signal
+import functools 
 
 HOST ='localhost'
 PORT = 8765
+
+#todo 
+# 1. Add ability to leave chat room
+
 
 class ClientAlreadyExistsException(Exception):
     pass
@@ -11,17 +17,25 @@ class ClientAlreadyExistsException(Exception):
 class ClientNotRegisteredInRoomException(Exception):
     pass
 
+class Message():
+    def __init__(self, author = None, text = None, targets = None):
+        self.author = author 
+        self.text = text
+        self.targets = targets
+
 class Client():
     def __init__(self, websocket=None,username=None, room=None):
         self.websocket = websocket
         self.username = username
-        self.room = None
+        self.room = room
 
     @property
     def chat_name(self):
         return self.username
 
 class Room():
+    chat_name = 'GLOBAL'
+
     def __init__(self, server=None, loop=None, messages = None, clients = None, uid = None, _name = None):
         self.server = server
         self.loop = loop or asyncio.get_event_loop()
@@ -39,98 +53,141 @@ class Room():
         self._name = val
 
     def readable_history(self, client):
-        history = '\n'.join(['{}: {}'.format(msg[0].chat_name, msg[1]) for msg in self.messages if not msg[2] or client in msg[2]])
-        return history
+        message_history = ['{}: {}'.format(message.author.chat_name, message.text) for message in self.messages if not message.targets or client in message.targets] # BUG currently on reconnection user wont get messages that were targeted at him during previous session
+        return '\n'.join(message_history)
 
     def is_command(self, message):
-        return message[:2] == '::' 
+        return message.text.startswith('::') 
 
     def get_client(self, websocket):
         for client in self.clients.copy():
             if client.websocket == websocket:
-
                 return client
         return False
 
+    def preprocess_command(self, message):
+        client = message.author
+        text = message.text[2:]
+        command_components = text.split(' ')
+        command = command_components[0]
+        args = command_components[1:]
+        return command, args
+
     async def on_client_joined(self, client):
-        await self.send_message(self, "Welcome to the {}! Here's what happened before you came:".format(self.name), [client], log = False)
+        message = Message(self, "Welcome to the {}! Here's what happened before you came:".format(self.name))
+        await self.send_message(message, log=False)
         history = self.readable_history(client)
         if history:
             await self.send_text(history, [client])
-        await self.send_message(self, '{} connected'.format(client.username))
+        message = Message(self, '{} connected'.format(client.username))
+        await self.send_message(message)
 
     async def on_client_disconnected(self, client):
-        await self.send_message(self, '{} disconnected'.format(client.username))
+        message = Message(self, '{} disconnected'.format(client.username))
+        await self.send_message(message)
 
     async def handle_command(self, client, message):
-        await self.send_message(self, 'Unrecognized command', [client], log = False) 
+        message = Message(self, 'Unrecognized command', targets=[client])
+        await self.send_message(message, log = False) 
 
-    async def handle_message(self, client, message):
+    async def handle_message(self, client, text):
+        message = Message(client, text)
         if not self.get_client(client.websocket):
             raise(ClientNotRegisteredInRoomException())
 
         if self.is_command(message):
-            await self.handle_command(client, message[2:])
+            await self.handle_command(message)
         else:
-            await self.send_message(client, message)
+            await self.send_message(message)
 
-    async def register_client(self, websocket, username):
+    async def register_client(self, client):
         try:
-            client = self.get_client(websocket)
-            if not client:
-                client = Client(websocket, username)
             self.clients.add(client)
             client.room = self
             await self.on_client_joined(client)
             return client
         except ClientAlreadyExistsException as e:
-            await self.server.send('Client already registered', websocket)
+            await self.server.send('Client already registered', client.websocket)
 
 
-    async def remove_client(self, websocket):
-        for client in self.clients.copy():
-            if client.websocket == websocket:
+    async def remove_client(self, client):
+        for c in self.clients.copy():
+            if c.websocket == client.websocket:
                 self.clients.remove(client)
                 await self.on_client_disconnected(client)
                 break
+        
 
-    async def send_text(self, message, targets): #Sending text doesn't show an author and is never logged
+
+    async def send_text(self, text, targets): #Sending text doesn't show an author and is never logged
         if not targets:
-            targets = self.clients #If no target is set its a global message
-
-        sending_list = [self.server.send("{}".format(message), client) for client in targets]
+            targets = self.clients
+        sending_list = [self.server.send("{}".format(text), client.websocket) for client in targets]
         if sending_list:
             done, pending = await asyncio.wait(sending_list)
 
-    async def send_message(self, author, message, targets=[], log = True):
+    async def send_message(self, message, log = True):
+        targets = message.targets
+        author = message.author
+        text = message.text
         if not targets:
-            targets = self.clients #If no target is set its a global message
-
-        sending_list = [self.server.send("{}: {}".format(author.chat_name, message), client) for client in targets]
+            targets = self.clients #If no target is set its a global (room) message
+        sending_list = [self.server.send("{}: {}".format(author.chat_name, text), client.websocket) for client in targets]
         if sending_list:
             done, pending = await asyncio.wait(sending_list)
         if log:
-            self.messages.append((author, message, targets))
+            self.messages.append(message)
 
-    @property
-    def chat_name(self):
-        return "GLOBAL"
+    
+class SubRoom(Room):
+    async def remove_client(self, client):
+        await super().remove_client(client)
+        if not self.clients: # Suicide
+            self.server.rooms.remove(self)
+            self = None
 
-class ChatRoom(Room):
+class ChatRoom(SubRoom):
+    chat_name = 'Chat'
 
-    @property
-    def chat_name(self):
-        return "Chat"
+
+    async def handle_command(self, message):
+        client = message.author
+        text = message.text
+        command, args = self.preprocess_command(message)
+
+        async def handle_leave(*args):
+            max_args_len = 0
+            if len(args) > max_args_len:
+                return "Too many arguments, expected {}".format(max_args_len)
+
+            await client.room.remove_client(client)
+            await self.server.room.register_client(client) #back to lobby
+            return 0
+
+        available_commands = {
+            "leave":handle_leave,
+        }
+
+        error_message = Message(author=self, targets=[client])
+        if command in available_commands.keys():
+            result = await available_commands[command](*args)
+            if result:
+                error_message.text = 'Error: {}'.format(result)
+        else:
+            error_message.text =  'Unrecognized command'
+
+        if error_message.text:
+            await self.send_message(error_message, log = False)
+
 
 
 class LobbyRoom(Room):
-    async def handle_command(self, client, message):
-        command_components = message.split(' ')
-        command = command_components[0]
-        args = command_components[1:]
+    chat_name = 'Lobby'
 
-
-
+    async def handle_command(self, message):
+        client = message.author
+        text = message.text
+        command, args = self.preprocess_command(message)
         async def handle_join(*args):
             max_args_len = 1
             if len(args) > max_args_len:
@@ -146,8 +203,8 @@ class LobbyRoom(Room):
             if not new_room:
                 return "There is no room with name {}.".format(room_name)
 
-            await new_room.register_client(client.websocket, client.username)
-            await client.room.remove_client(client.websocket)
+            await client.room.remove_client(client)
+            await new_room.register_client(client)
 
             return 0 
             
@@ -165,28 +222,26 @@ class LobbyRoom(Room):
 
             new_room = ChatRoom(self.server, self.loop, _name=room_name)
             
-            await new_room.register_client(client.websocket, client.username)
-            await client.room.remove_client(client.websocket)
+            await client.room.remove_client(client)
+            await new_room.register_client(client)
             self.server.rooms.append(new_room)
             return 0
         
         available_commands = {
             "join":handle_join,
-            "create":handle_create
+            "create":handle_create,
         }
 
+        error_message = Message(author=self, targets=[client])
         if command in available_commands.keys():
             result = await available_commands[command](*args)
             if result:
-                await self.send_message(self, 'Error: {}'.format(result), [client], log = False) 
+                error_message.text = 'Error: {}'.format(result)
         else:
-            await self.send_message(self, 'Unrecognized command', [client], log = False) 
+            error_message.text =  'Unrecognized command'
 
-    @property
-    def chat_name(self):
-        return "Lobby"
-
-
+        if error_message.text:
+            await self.send_message(error_message, log = False)
 
 class ChatServer:
     def __init__(self, host=HOST, port=PORT, loop=None, messages = None, clients = None, rooms = None):
@@ -210,11 +265,8 @@ class ChatServer:
             return client
         return None
 
-    async def send(self, message, client):
-        if type(client) == Client:
-            await client.websocket.send(message)
-        else:
-            await client.send(message)
+    async def send(self, text, websocket):
+        await websocket.send(text)
 
     async def handler(self, websocket, path):
         client = None
@@ -222,24 +274,42 @@ class ChatServer:
             try:
                 client = self.get_client(websocket)
                 if not client:
-                    await websocket.send('Please register')
+                    client = Client(websocket=websocket)
+                    await websocket.send('Please register by typing your username')
                     username = await websocket.recv()
-                    client = await self.room.register_client(websocket, username)
+                    client.username = username
+                    await self.room.register_client(client)
                     
-                message = await websocket.recv()
-                response = await client.room.handle_message(client, message)
+                text = await websocket.recv()
+                response = await client.room.handle_message(client, text)
 
             except websockets.exceptions.ConnectionClosed as e:
                 if client and client.room:
-                    await client.room.remove_client(websocket)                    
+                    await client.room.remove_client(client)                    
                 break
 
     def run(self):
-        self.loop.run_until_complete(websockets.serve(self.handler, self.host, self.port))
+        self.websocket_server = websockets.serve(self.handler, self.host, self.port)
+        self.loop.run_until_complete(self.websocket_server)
+        asyncio.async(wakeup()) #HACK so keyboard interrupt works on Windows
         self.loop.run_forever()
+        self.loop.close()
+        self.clean_up()
 
+    def clean_up(self):
+        self.websocket_server.close()
+        for task in asyncio.Task.all_tasks():
+            task.cancel()
+
+
+async def wakeup(): # HACK  http://stackoverflow.com/questions/27480967/why-does-the-asyncios-event-loop-suppress-the-keyboardinterrupt-on-windows
+
+    while True:
+        await asyncio.sleep(1) 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     chat = ChatServer(loop=loop)
+
+
     chat.run()
